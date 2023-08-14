2527a70d9be5d173475fec9e63aceb070e27c329
xiechao6 <xiechao6@xiaomi.com>
2023-08-14 15:37:36 +0800

    feat:配置依赖

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="box"><h1>我是APP</h1></div>
+</template>
+<script setup lang="ts">
+//  import { ref } from "vue";
+</script>
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 600px;
+  background: #000;
+  h1 {
+    color: rgb(253, 252, 252);
+  }
+}
+</style>
