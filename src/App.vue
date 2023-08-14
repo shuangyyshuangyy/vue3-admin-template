6244b4df2193614dc7378a1eb0c0c29ebeca1c01
xiechao6 <xiechao6@xiaomi.com>
2023-08-14 18:58:01 +0800

    feat: 全局引入element-plus组件库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="box"><h1>我是APP</h1></div>
+  <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+  <el-button type="success" size="default" :icon="Edit">编辑按钮</el-button>
+  <el-pagination background layout="prev, pager, next" :total="1000" />
+
+  <div class="demo-pagination-block">
+    <div class="demonstration">All combined</div>
+    <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
 </template>
 <script setup lang="ts">
-//  import { ref } from "vue";
+import { ref } from 'vue';
+import { Plus, Edit } from '@element-plus/icons-vue';
+const currentPage4 = ref(4);
+const pageSize4 = ref(100);
+const small = ref(false);
+const background = ref(false);
+const disabled = ref(false);
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
 </script>
 <style scoped lang="scss">
 .box {
