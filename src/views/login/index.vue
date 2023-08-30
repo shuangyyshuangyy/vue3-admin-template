ff40cf3f6fa75ebe71c4d1a7523438a370786369
xiechao6 <xiechao6@xiaomi.com>
2023-08-30 19:39:02 +0800

    feat: 表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,13 +4,18 @@
     <el-row>
       <el-col :span="12" :xs="0">{{ userStore.token }}</el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="form">
+        <el-form
+          ref="loginFormRef"
+          :model="formState"
+          :rules="formRules"
+          class="form"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input :prefix-icon="User" v-model="formState.username" />
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               type="password"
               :prefix-icon="Lock"
@@ -41,7 +46,7 @@ import { reactive, ref } from 'vue';
 import { useUserStore } from '@/store/modules/use-user-store';
 import type { loginData } from '@/api/user/type';
 import { useRouter } from 'vue-router';
-import { ElNotification } from 'element-plus';
+import { ElNotification, FormRules } from 'element-plus';
 import { getTime } from '@/utils/time';
 
 //收集表单信息
@@ -50,11 +55,70 @@ const formState: loginData = reactive({
   password: '111111',
 });
 
-//获取路由器
-const router = useRouter();
+//自定义校验password规则
+const checkPassword = (
+  _rule: any,
+  value: string, //value:表单元素的文本内容
+  callback: (arg0?: Error) => void,
+) => {
+  let regExp = /^[a-zA-Z0-9_]+$/; //password只能为字母数字下划线
+  if (regExp.test(value)) {
+    callback();
+  } else {
+    callback(new Error('密码错误'));
+  }
+};
+
+//表单
+const formRules = reactive<FormRules>({
+  username: [
+    //一个对象代表一条规则
+    {
+      required: true,
+      message: '用户名不能为空',
+      //失去焦点时校验
+      trigger: 'blur',
+    },
+  ],
+  password: [
+    {
+      min: 6,
+      max: 10,
+      message: '密码长度6~10位',
+      trigger: 'change',
+    },
+    {
+      validator: checkPassword,
+      //内容改变时校验
+      trigger: 'blur',
+    },
+  ],
+});
+
+const router = useRouter(); //获取路由器
 const userStore = useUserStore();
 const loading = ref<boolean>(false);
+const loginFormRef = ref();
+
+//对整个表单的内容进行验证
+const validData = async () => {
+  try {
+    await loginFormRef.value.validate();
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+//重置表单，将其值重置为初始值，并移除校验结果
+// const resetForm = () => {
+//   loginFormRef.value.resetFields();
+// };
+
+//登录
 const Login = async () => {
+  const valid = await validData();
+  if (!valid) return;
   loading.value = true;
   try {
     let message = await userStore.login(formState);
